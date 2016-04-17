51456e234aef6305e99fa9890bb57d7d4edc2b65
musikisomorphie <wujiqing9@gmail.com>
2016-04-17 17:43:29 +0200

    configure dependencies

---
+++ Setpath.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+root=/scratch_net/avatar/jwu/.usr/local
+MKL_DIR=$root/intel
+
+export MANPATH=$MANPATH:$root/share  
+export PATH=$PATH:$root/bin 
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$root/include 
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:$root/include 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$root/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$root/lib/i386-linux-gnu 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$root/intel/mkl/lib/intel64
+export LIBRARY_PATH=$LIBRARY_PATH:$root/lib
+
+export BLAS_INCLUDE=$BLAS_INCLUDE:$MKL_DIR/include
+export BLAS_LIB=$BLAS_LIB:$MKL_DIR/lib 
+export BLAS_LIB=$BLAS_LIB:$MKL_DIR/lib/intel64
+export HDF5_DISABLE_VERSION_CHECK=1
